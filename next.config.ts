28620735c9af3c71e7bab5552841f1fb7d249daa
yuto takezawa <y123ta1945kari@gmail.com>
2026-05-27 06:40:37 +0900

    coop許可

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,23 @@
-// next.config.ts
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['mongoose'],
   turbopack: {
     resolveExtensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  //  ここから追加：ポップアップの通信を許可するヘッダー設定
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // すべてのページとAPIルートに適用
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups', // ポップアップの操作を許可
+          },
+        ],
+      },
+    ]
   },
 }
 
