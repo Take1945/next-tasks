d39178f687d733e346c93a569b73b55ee74990bb
yuto takezawa <y123ta1945kari@gmail.com>
2026-05-27 11:09:56 +0900

    syuusei

--- app/(main)/completed/page.tsx
+++ app/(main)/completed/page.tsx
@@ -1,44 +1,28 @@
+import TaskCard from "@/app/components/TaskCard/TaskCard"
+import { TaskDocument, TaskModel } from "@/app/src/models/task"
+import { connectDb } from "@/app/src/units/database"
 
-import TaskCard from "@/app/components/TaskCard/TaskCard" 
-import { TaskDocument } from "@/app/src/models/task";
-import { cookies } from "next/headers";
-
-const getAllTasks = async():Promise<TaskDocument[]>=>{
-  const cookieStore =await cookies()
-const response =await fetch(`${process.env.API_URL}/tasks`,{
-  cache:'no-store',
-  headers:{
-     Cookie: cookieStore.getAll().map(c => `${c.name}=${c.value}`).join('; ')
-   
-  },
-});
-if(response.status!== 200){
-  throw new Error()
+const getAllTasks = async (): Promise<TaskDocument[]> => {
+  await connectDb()
+  const allTasks = await TaskModel.find()
+  return allTasks
 }
-const data = await response.json();
-return data.tasks as TaskDocument[];
-}
-const CompletedTaskPage = async() => {
 
-  const allTask =await getAllTasks()
-
-  const filterAlltask = allTask.filter((task)=>{
-  const dueDate = new Date(task.dueDate)
-return (
-  dueDate && task.isCompleted===true
-)
-  })
-  
+const CompletedTaskPage = async () => {
+  const allTask = await getAllTasks()
+  const filterAlltask = allTask.filter((task) => task.isCompleted === true)
 
   return (
-   <div className='text-gray-800 p-8 h-full overflow-y-auto pb-24'>
-  <header className='flex justify-between items-center'>
-    <h1 className='text-2xl font-bold flex items-center'>Completed Tasks</h1>
-  </header>
-  <div className='mt-8 flex flex-wrap gap-4'>
-   {filterAlltask?.map((task)=>task&&<TaskCard key={task._id.toString()} task={task} />)}
-  </div>
-</div>
+    <div className='text-gray-800 p-8 h-full overflow-y-auto pb-24'>
+      <header className='flex justify-between items-center'>
+        <h1 className='text-2xl font-bold'>Completed Tasks</h1>
+      </header>
+      <div className='mt-8 flex flex-wrap gap-4'>
+        {filterAlltask?.map((task) => task &&
+          <TaskCard key={task._id.toString()} task={task} />
+        )}
+      </div>
+    </div>
   )
 }
 
